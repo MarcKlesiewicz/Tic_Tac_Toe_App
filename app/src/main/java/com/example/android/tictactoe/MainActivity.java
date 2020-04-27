e6d882f6bf2e5ea24a668311eb1b18a5602b0cf6
MarcKlesiewicz <marcklesiewicz21@gmail.com>
2020-04-27 12:05:03 +0200

    resetboard & update point text

--- app/src/main/java/com/example/android/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -119,10 +120,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void player1Wins() {}
+    private void player1Wins() {
+        player1Point++;
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
 
-    private void player2Win(){}
+    private void player2Win(){
+        player2Point++;
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
 
-    private void draw(){}
+    private void draw(){
+        Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void updatePointsText(){
+        textViewPlayer1.setText("Player 1: " + player1Point);
+        textViewPlayer2.setText("player 2: " + player2Point);
+    }
+    private void resetBoard(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
 
 }
